3ed40635f90091e73c371c5d0b9f94fa267a6c13
RinaUchiyama <rina@RinaUchiyamanoMacBook.local>
2020-01-23 13:12:27 +0900

    画像のループ表示できた

---
+++ main.js
@@ -0,0 +1,31 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+
+images = [
+    "images/pic1.jpg",
+    "images/pic2.jpg",
+    "images/pic3.jpg",
+    "images/pic4.jpg",
+    "images/pic5.jpg",
+];
+for(let i =0; i <= images.length - 1; i++) {
+    image = images;
+    // console.log(image[i]);
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', image[i]);
+    thumbBar.appendChild(newImage);
+
+    newImage.onclick = function (){
+        console.log("hi");
+    };
+    // newImage.setAttribute('onclick', changeImage());
+};
+
+
+
+/* Wiring up the Darken/Lighten button */
