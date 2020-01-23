a9e87683739ccec2927fe5d6190ebf799c5cbf64
RinaUchiyama <rina@RinaUchiyamanoMacBook.local>
2020-01-23 13:17:59 +0900

    初期状態

--- main.js
+++ main.js
@@ -6,26 +6,8 @@ const overlay = document.querySelector('.overlay');
 
 /* Looping through images */
 
-images = [
-    "images/pic1.jpg",
-    "images/pic2.jpg",
-    "images/pic3.jpg",
-    "images/pic4.jpg",
-    "images/pic5.jpg",
-];
-for(let i =0; i <= images.length - 1; i++) {
-    image = images;
-    // console.log(image[i]);
-    const newImage = document.createElement('img');
-    newImage.setAttribute('src', image[i]);
-    thumbBar.appendChild(newImage);
-
-    newImage.onclick = function (){
-        console.log("hi");
-    };
-    // newImage.setAttribute('onclick', changeImage());
-};
-
-
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
